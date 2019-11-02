969afe3e9b230745c457e51d60b6e264068673cc
Boris Vogt <boris.vogt@posteo.de>
2019-11-02 13:50:05 +0100

    BCN-FT-Boris Vogt ./css/styles.css added basic styles

--- src/player.js
+++ src/player.js
@@ -8,15 +8,15 @@ function Player( canvas, lives ) {
     this.lives = lives; // defining lives property
 
     this.sizeWidth = 30;    // defining playerWidth
-    this.sizeHeigth = 70;  // defining playerHeigth
+    this.sizeHeight = 70;  // defining playerHeight
     this.x = 50;    // defining player X default position
-    this.y = 120;    // defining player Y default position
+    this.y = 100;    // defining player Y default position
 
 }
 
 // defining Player prototype draw function
 Player.prototype.draw = function() {
     this.ctx.fillStyle = '#33FFF0'; // color property
-    // defining player x position, player y position, player width, player heigth
-    this.ctx.fillRect(this.x, this.y, this.sizeWidth, this.sizeHeigth); 
+    // defining player x position, player y position, player width, player height
+    this.ctx.fillRect(this.x, this.y, this.sizeWidth, this.sizeHeight); 
 }
