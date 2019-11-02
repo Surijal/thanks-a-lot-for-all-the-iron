8791e5dbb064cfe3efd47d98b8085152a4bc4e9d
Boris Vogt <boris.vogt@posteo.de>
2019-11-02 11:28:46 +0100

    BCN-FT-Boris Vogt modified src/game.js function Game, canvas gameSreen property added

---
+++ src/game.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// defining Game properties 
+function Game() {
+    this.canvas = null; //add canvas property
+    this.ctx = null;    //add ctx property
+    // add enemy property
+    // add player property
+    this.gameScreen = null;
+}
