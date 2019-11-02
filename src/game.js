5bd5de3be0215c6b7dabd46a4ee35eff6d83a82c
Boris Vogt <boris.vogt@posteo.de>
2019-11-02 11:42:38 +0100

    BCN-FT-Boris Vogt modified src/game.js Game.prototype function definend, basic viewport propertys added

--- src/game.js
+++ src/game.js
@@ -8,3 +8,18 @@ function Game() {
     // add player property
     this.gameScreen = null;
 }
+
+// defining Game prototype function
+Game.prototype.start = function() {
+    this.canvasContainer = document.querySelector('.canvas-container'); // get canvas-container
+    this.canvas = document.querySelector('canvas'); // get canvas
+    this.ctx = this.canvas.getContext('2d'); // defining Canvas Context 2d
+
+    // defining the canvas viewport
+    this.containerWidth = this.canvasContainer.offsetWidth; // defining canvas width
+    this.containerHeigth = this.canvasContainer.offsetHeigth;   // defining canvas heigth
+    this.canvas.setAttribute('width', this.containerWidth ); // adding width attribute to containerWidth
+    this.canvas.setAttribute('heigth', this.containerHeigth ); // adding height attribute to containerHeigth
+    
+    // call startLoop - starting the game Loop
+}
