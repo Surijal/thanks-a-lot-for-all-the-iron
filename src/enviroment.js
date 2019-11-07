b517f0c8a3460eb000037a817576e0a91fe3556f
Boris Vogt <boris.vogt@posteo.de>
2019-11-07 15:43:27 +0100

    border add

--- src/enviroment.js
+++ src/enviroment.js
@@ -24,4 +24,21 @@ this.ctx.fillStyle = pattern;
 this.ctx.fillRect(0, this.groundY, this.canvas.width, this.groundY);
 }
 
+function sky  (canvas) {
+    this.canvas = canvas; // defines canvas of Ground
+    this.ctx = this.canvas.getContext('2d'); //defining Ground canvas 2d
 
+    this.groundWidth = this.canvas.width;
+    this.groundY = this.canvas.height - 76;
+    
+    this.image = new Image();
+    this.image.src = '/image/sky.png';
+}
+
+Sky.prototype.draw = function () {
+    this.ctx.fillRect(0, 0, this.canvas.width, this.groundY);
+    this.ctx.stroke();
+    var pattern = this.ctx.createPattern(this.image, 'repeat');
+    this.ctx.fillStyle = pattern;
+    this.ctx.fillRect(0, this.groundY, this.canvas.width, this.groundY);
+}
