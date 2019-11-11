072bc5d2e9c4af01dccd9d8b63b0e91478b36a91
Boris Vogt <boris.vogt@posteo.de>
2019-11-11 17:15:16 +0100

    refactored .src/enemy/ draw Enemy

--- src/enemy.js
+++ src/enemy.js
@@ -16,42 +16,21 @@ class SpikedEnemy {
             this.x= startX
             this.image = new Image();
         }
+        
+        drawSpikedEnemyLeft  () {
+        
+            if ( this.direction === -1 ) {
+                this.image.src = "./image/enemy-right.png"
+                this.ctx.drawImage(this.image, this.x, this.y, this.spikedEnemyWidth, this.spikedEnemyHeight)
+            } else {
+                this.image.src = "./image/enemy-left.png"
+                this.ctx.drawImage(this.image, this.x, this.y, this.spikedEnemyWidth, this.spikedEnemyHeight)
+        
+            }
+
+        }
 }
 
-// function SpikedEnemy ( canvas,  speed, startX, direction) {
-//     this.canvas = canvas;
-//     this.ctx = canvas.getContext('2d');
-
-//     this.canvasWidth = this.canvas.width;
-//     this.canvasHeight = this.canvas.height;
-//     this.groundHeight = 76;
-
-//     // this.startLeft = startLeft;
-//     // this.startRight = startRight;
-
-//     this.spikedEnemyHeight = 25;
-//     this.spikedEnemyWidth = 32;
-//     this.direction=direction;
-//     this.speed = speed;
-//     this.y = this.canvasHeight - this.groundHeight - this.spikedEnemyHeight;
-//     this.x= startX
-//     this.image = new Image();
-//     // this. x = 0;
-
-// }
-SpikedEnemy.prototype.drawSpikedEnemyLeft = function () {
-    // this.ctx.fillStyle ='#581845';
-
-    if ( this.direction === -1 ) {
-        this.image.src = "./image/enemy-right.png"
-        this.ctx.drawImage(this.image, this.x, this.y, this.spikedEnemyWidth, this.spikedEnemyHeight)
-    } else {
-        this.image.src = "./image/enemy-left.png"
-        this.ctx.drawImage(this.image, this.x, this.y, this.spikedEnemyWidth, this.spikedEnemyHeight)
-
-    }
-    // this.ctx.fillRect( this.x, this.y, this.spikedEnemyWidth, this.spikedEnemyHeight);
-}
 
 SpikedEnemy.prototype.drawSpikedEnemyRight = function () {
     this.ctx.fillStyle ='#581845';
