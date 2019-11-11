887e3af74c1e55073f83062b99d7f4d3845c4f24
Boris Vogt <boris.vogt@posteo.de>
2019-11-11 17:01:23 +0100

    refactore ./src/enviroment sky prototype and call to ES6

--- src/enviroment.js
+++ src/enviroment.js
@@ -35,23 +35,12 @@ class Sky {
             this.image = new Image();
             this.image.src = './image/sky.jpg';
         }
+            draw () {
+                this.ctx.fillRect(0, 0, this.canvas.width, this.groundY);
+                this.ctx.stroke();
+                var pattern = this.ctx.createPattern(this.image, 'repeat');
+                this.ctx.fillStyle = pattern;
+                this.ctx.fillRect(0,  0, this.canvas.width, this.groundY);
+        }
 }
 
-// function Sky  (canvas) {
-//     this.canvas = canvas; // defines canvas of Ground
-//     this.ctx = this.canvas.getContext('2d'); //defining Ground canvas 2d
-
-//     this.groundWidth = this.canvas.width;
-//     this.groundY = this.canvas.height - 76;
-    
-//     this.image = new Image();
-//     this.image.src = './image/sky.jpg';
-// }
-
-Sky.prototype.draw = function () {
-    this.ctx.fillRect(0, 0, this.canvas.width, this.groundY);
-    this.ctx.stroke();
-    var pattern = this.ctx.createPattern(this.image, 'repeat');
-    this.ctx.fillStyle = pattern;
-    this.ctx.fillRect(0,  0, this.canvas.width, this.groundY);
-}
