d8fa208e79c7a71be14043ab1135c9b40220e8da
Boris Vogt <boris.vogt@posteo.de>
2019-11-11 17:03:44 +0100

    refactored .src/goods/ Ironbaras constructor

--- src/goods.js
+++ src/goods.js
@@ -1,23 +1,26 @@
 'use strict';
 
 // defining ironbar property function
-function Ironbars ( canvas, x, speed, random ) {
-    this.canvas = canvas; 
-    this.ctx = canvas.getContext('2d');
+class Ironbars {
+    constructor ( canvas, x, speed, random ) {
+        this.canvas = canvas; 
+        this.ctx = canvas.getContext('2d');
+    
+        this.canvasHeight = this.canvas.height;
+        this.groundHeight = 76;
+    
+        this.speed = speed;
+        this.random = random;
+    
+        this.ironbarHeight = 24;
+        this.ironbarWidth = 24;
+    
+        this.x = x;
+        this.y = this.canvasHeight - this.groundHeight - (this.ironbarHeight * 4);
+    
+        this.image = new Image();
+    }
 
-    this.canvasHeight = this.canvas.height;
-    this.groundHeight = 76;
-
-    this.speed = speed;
-    this.random = random;
-
-    this.ironbarHeight = 24;
-    this.ironbarWidth = 24;
-
-    this.x = x;
-    this.y = this.canvasHeight - this.groundHeight - (this.ironbarHeight * 4);
-
-    this.image = new Image();
 }
 
 Ironbars.prototype.drawIronbars = function() {
